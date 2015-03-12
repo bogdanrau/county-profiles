0c1f3db186a3108cfcd28cdbeb1287c202535182
bogdanrau <bogdan.rau@gmail.com>
2015-03-11 17:47:09 -0700

    Bugfix & data
    Fixed a bug where downloading charts would not always work. Added more
    files to the dataset.

--- server.R
+++ server.R
@@ -17,7 +17,7 @@ shinyServer(function(input, output, session) {
             data <- isolate( ## Create data by concatenating different values from the input fields
                 read.csv(file = paste("data/", input$year, "/", input$population, "/", input$locationType, "/",
                                       input$coLocation, ".csv", sep=""), check.names = FALSE)
-                )
+            )
             print(data, type='html') # Print data to console to show on screen
             
             
@@ -32,11 +32,11 @@ shinyServer(function(input, output, session) {
         columnDefs =  list(
             list(targets = c(2), className = 'alignCenter'),
             list(targets = c(1), className = 'highlight')
-            )
-            
-            
         )
-
+        
+        
+    )
+    
     
     )
     
@@ -48,48 +48,61 @@ shinyServer(function(input, output, session) {
             
             
             insurance <- isolate(read.csv(file = paste("data/", input$year, "/", input$population, "/", input$locationType , "/",
-                                      input$coLocation, ".csv", sep=""), check.names = FALSE, nrows=4, skip=3, header = FALSE, colClasses=c(NA, NA, "NULL")))
+                                                       input$coLocation, ".csv", sep=""), check.names = FALSE, nrows=4, skip=3, header = FALSE, colClasses=c(NA, NA, "NULL")))
             insurance[['V2']] <- as.numeric(sub("%", "", insurance[['V2']]))
             insurance2 <- as.matrix(insurance)
             values <- insurance[,'V2']
             opar <- par(lwd = 0.1)
             mp <- barplot(insurance[,'V2'],
-                    main="Insurance Status",
-                    ylab="Adults aged 18-64 (%)",
-                    ylim= range(pretty(c(0, values))),
-                    xlab="Health Insurance",
-                    names.arg=c("Uninsured", "Employer-Based", "Medi-Cal/HF", "Other"),
-                    col=c("indianred2", "lightskyblue3", "lightskyblue3", "lightskyblue3"),
-                    xpd=FALSE)
-            text(mp, values, labels=values, pos=3, offset=.3, xpd=FALSE, font=2)
+                          main="Insurance Status",
+                          ylab="Adults aged 18-64 (%)",
+                          ylim= range(pretty(c(0, values))),
+                          xlab="Health Insurance",
+                          names.arg=c("Uninsured", "Employer-Based", "Medi-Cal/HF", "Other"),
+                          col=c("indianred2", "lightskyblue3", "lightskyblue3", "lightskyblue3"),
+                          xpd=FALSE)
+            text(mp, values, labels=values, pos=3, offset=-1.5, xpd=FALSE, font=2, col="white")
             abline(h=0, col="black", lwd=1)
-
-    }})
+            
+        }})
     
     output$downloadPlot <- downloadHandler(
         # Specify the file name
         filename = function() {
-            paste("chart.png")
+            paste(input$coLocation, "png", sep=".")
         },
         content = function(file) {
             # open the device
             png(file, width = 580, height = 400)
             # create the plot
+            input$getResults
+            if(input$getResults == '0') { return() }
+            else {
+                
+                
+                insurance <- isolate(read.csv(file = paste("data/", input$year, "/", input$population, "/", input$locationType , "/",
+                                                           input$coLocation, ".csv", sep=""), check.names = FALSE, nrows=4, skip=3, header = FALSE, colClasses=c(NA, NA, "NULL")))
+                insurance[['V2']] <- as.numeric(sub("%", "", insurance[['V2']]))
+                insurance2 <- as.matrix(insurance)
+                values <- insurance[,'V2']
+                opar <- par(lwd = 0.1)
                 mp <- barplot(insurance[,'V2'],
-                      main="Insurance Status",
-                      ylab="Adults aged 18-64 (%)",
-                      ylim= range(pretty(c(0, values))),
-                      xlab="Health Insurance",
-                      names.arg=c("Uninsured", "Employer-Based", "Medi-Cal/HF", "Other"),
-                      col=c("indianred2", "lightskyblue3", "lightskyblue3", "lightskyblue3"),
-                      xpd=FALSE)
-                      text(mp, values, labels=values, pos=3, offset=.3, xpd=FALSE, font=2)
-                      abline(h=0, col="black", lwd=1)
-                 
+                              main="Insurance Status",
+                              ylab="Adults aged 18-64 (%)",
+                              ylim= range(pretty(c(0, values))),
+                              xlab="Health Insurance",
+                              names.arg=c("Uninsured", "Employer-Based", "Medi-Cal/HF", "Other"),
+                              col=c("indianred2", "lightskyblue3", "lightskyblue3", "lightskyblue3"),
+                              xpd=FALSE)
+                text(mp, values, labels=values, pos=3, offset=-1.5, xpd=FALSE, font=2, col="white")
+                abline(h=0, col="black", lwd=1)
+                
+            }
+            
             # close the device
             dev.off()
         }
-        )
+    )
     
     
 })
